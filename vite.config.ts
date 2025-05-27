ba93630173117e1ed795ba2eeeb5854f46367c83
nsainani <nsainani@baraq.com>
2025-05-27 16:00:12 +0530

    yo my life

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace with your actual GitHub repo name
-const repoName = 'menepidemic.github.io';
-
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? `/${repoName}/` : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
@@ -17,8 +14,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
